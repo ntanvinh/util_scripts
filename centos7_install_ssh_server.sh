cf8c63d486e8f08feae24a0ff6171acc882cf458
Vinh <ntanvinh@gmail.com>
2022-03-20 06:02:52 +0700

    fix firewall arguments

--- centos7_install_ssh_server.sh
+++ centos7_install_ssh_server.sh
@@ -1,12 +1,12 @@
-# install ssh daemon
+echo INSTALL SSH DAEMON
 yum update -y
 yum install -y openssh openssh-server openssh-clients openssl-libs
 systemctl start sshd
-
-# open firewall
-firewall-cmd --zone=public --add-port=22/tcp –permanent
-firewall-cmd –reload
-
-# check status
+echo
+echo OPEN FIREWALL
+firewall-cmd --zone=public --add-port=22/tcp --permanent
+firewall-cmd --reload
+echo
+echo CHECK STATUS
 yum install net-tools -y 
 netstat -nutlp | grep 22
