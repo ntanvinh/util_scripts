7a884d2860546f12c0bfa9ca6a8a8204fee8b19c
vinhnt <ntanvinh@gmail.com>
2022-06-10 22:20:07 +0700

    change mongodb version to compatible with centos 7

--- cen7_install_deploy_softwares.sh
+++ cen7_install_deploy_softwares.sh
@@ -9,17 +9,17 @@ sudo systemctl enable docker
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm install node 12
+exec bash
+nvm install 12
 nvm use 12
 
 # mongodb
-sudo tee /etc/yum.repos.d/mongodb-org-5.0.repo<<EOF
-[mongodb-org-5.0]
+sudo tee /etc/yum.repos.d/mongodb-org.repo<<EOF
+[mongodb-org-4.4]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/5.0/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.4/x86_64/
 gpgcheck=1
-enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc
+gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
 EOF
 
 sudo yum install mongodb-org
