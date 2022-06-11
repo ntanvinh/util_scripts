e9a557baec48dd1f4b772d3067458fd9d2609015
vinhnt <ntanvinh@gmail.com>
2022-06-11 07:24:50 +0700

    fix install nvm

--- cen7_install_deploy_softwares.sh
+++ cen7_install_deploy_softwares.sh
@@ -9,7 +9,9 @@ sudo systemctl enable docker
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-exec bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 12
 nvm use 12
 
