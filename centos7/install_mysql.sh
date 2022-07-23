ac0385712dab2c8db3b128f0ea80e6600c0dfdb1
vinhnt <ntanvinh@gmail.com>
2022-07-23 19:03:50 +0700

    add install mysql scrip

---
+++ centos7/install_mysql.sh
@@ -0,0 +1,8 @@
+curl -sSLO https://dev.mysql.com/get/mysql80-community-release-el7-6.noarch.rpm
+sudo rpm -ivh mysql80-community-release-el7-6.noarch.rpm
+sudo yum install mysql-server
+sudo systemctl start mysqld
+sudo systemctl status mysqld
+sudo grep 'temporary password' /var/log/mysqld.log
+sudo mysql_secure_installation
+mysqladmin -u root -p version
---
+++ macos/change_mac_address.sh
@@ -0,0 +1,4 @@
+OLD_MAC="ac:bc:32:9a:1a:09"
+NEW_MAC="14:98:77:88:3e:16"
+
+sudo ifconfig en0 $NEW_MAC
