8817a6ffb76e5c620e397fafcfe12c32039a46d9
vinhnt <ntanvinh@gmail.com>
2022-08-04 09:37:56 +0700

    add clean disk space for centos

--- centos7/install_mysql.sh
+++ centos7/install_mysql.sh
@@ -1,6 +1,6 @@
 curl -sSLO https://dev.mysql.com/get/mysql80-community-release-el7-6.noarch.rpm
 sudo rpm -ivh mysql80-community-release-el7-6.noarch.rpm
-sudo yum install mysql-server
+sudo yum install --nogpgcheck mysql-server
 sudo systemctl start mysqld
 sudo systemctl status mysqld
 sudo grep 'temporary password' /var/log/mysqld.log
