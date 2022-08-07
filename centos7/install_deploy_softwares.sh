01c30bb6dab8f451f67099dd3f15c1df2d849073
vinhnt <ntanvinh@gmail.com>
2022-08-07 18:41:57 +0700

    separate install mongodb

--- centos7/install_deploy_softwares.sh
+++ centos7/install_deploy_softwares.sh
@@ -18,8 +18,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 12
-nvm use 12
 
 # yarn
 curl --silent --location https://rpm.nodesource.com/setup_12.x | sudo bash -
@@ -27,17 +25,3 @@ sudo yum install -y nodejs
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 sudo rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
 sudo yum install -y yarn
-
-# mongodb
-sudo tee /etc/yum.repos.d/mongodb-org.repo<<EOF
-[mongodb-org-4.4]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.4/x86_64/
-gpgcheck=1
-enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
-EOF
-
-sudo yum install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
---
+++ centos7/install_mongodb.sh
@@ -0,0 +1,13 @@
+# mongodb
+sudo tee /etc/yum.repos.d/mongodb-org.repo<<EOF
+[mongodb-org-4.4]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.4/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
+EOF
+
+sudo yum install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
